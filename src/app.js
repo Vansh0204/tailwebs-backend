edbfba17b8d375f3b3b368443a495ef07cf2f0a2
Vansh0204 <vanshyt425@gmail.com>
2026-04-05 01:26:34 +0530

    Fix & Feature: Added PATCH to CORS methods and updated Diagnostic Status

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.get('/api/status', statusHandler);
 app.use(cors({
   origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 

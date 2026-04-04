c10c2f02f6a6f0a785197f0fd7de32be1915f999
Vansh0204 <vanshyt425@gmail.com>
2026-04-05 00:10:00 +0530

    Final API Polishing and Submission Attachment Support

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const authRoutes = require('./routes/authRoutes');
+const assignmentRoutes = require('./routes/assignmentRoutes');
+const submissionRoutes = require('./routes/submissionRoutes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/assignments', assignmentRoutes);
+app.use('/api/submissions', submissionRoutes);
+
+// Base Route
+app.get('/', (req, res) => {
+  res.send('Assignment Workflow API is running...');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
+module.exports = app;
